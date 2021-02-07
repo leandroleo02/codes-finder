362492c3152b4299b02f4be0d1ccf95461e6ebaf
Leandro Rodrigues <leandroleo02@gmail.com>
2021-02-06 21:09:41 -0300

    Prepare line for searching

--- runefinder.go
+++ runefinder.go
@@ -2,14 +2,56 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const (
 	ucd = "UnicodeData.txt"
 )
+
+func matchLine(line string, word string) bool {
+	return false
+}
+
+func splitWords(words string) []string {
+	return strings.Split(words, " ")
+}
+
+// PrepareLine analise the line and returns the fields
+func PrepareLine(line string) (rune, string, []string, error) { // TODO: line should be a struct?
+	if line == "" {
+		return 0, "", nil, errors.New("Empty Line")
+	}
+
+	fields := strings.Split(line, ";")
+	code, _ := strconv.ParseInt(fields[0], 16, 32)
+	name := fields[1]
+	words := splitWords(fields[1])
+
+	return rune(code), name, words, nil
+}
+
+// FindRunes search in the file for the words in the description
+func FindRunes(f *os.File, criteria string) []string {
+	scanner := bufio.NewScanner(f)
+	var runes []string
+	for scanner.Scan() {
+		line := scanner.Text()
+		words := splitWords(criteria)
+		for _, word := range words {
+			if matchLine(line, word) {
+				// TODO: format line
+				runes = append(runes, line)
+			}
+		}
+	}
+	return runes
+}
 
 func listContent(f *os.File) {
 	scanner := bufio.NewScanner(f)
@@ -28,11 +70,11 @@ func openUnicodeData() (*os.File, error) {
 }
 
 func main() {
-	file, err := openUnicodeData();
+	file, err := openUnicodeData()
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	listContent(file)
 	file.Close()
 }
--- runefinder_test.go
+++ runefinder_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,4 +11,29 @@ func TestReadFile(t *testing.T) {
 
 	assert.NoError(t, err)
 	assert.NotNil(t, file)
+}
+
+func TestFindRunes(t *testing.T) {
+	file, _ := openUnicodeData()
+
+	runes := FindRunes(file, "CHIPMUNK")
+	assert.Len(t, runes, 0)
+}
+
+func TestPrepareLineIgnoreEmptiness(t *testing.T) {
+	code, name, words, err := PrepareLine("")
+
+	assert.Equal(t, code, int32(0))
+	assert.Equal(t, name, "")
+	assert.Len(t, words, 0)
+	assert.Error(t, err)
+}
+
+func TestPrepareLineWithSingleWordDescription(t *testing.T) {
+	code, name, words, err := PrepareLine("1F43F;CHIPMUNK;So;0;ON;;;;;N;;;;;")
+
+	assert.NoError(t, err)
+	assert.Equal(t, code, rune(128063))
+	assert.Equal(t, name, "CHIPMUNK")
+	assert.Len(t, words, 1)
 }
